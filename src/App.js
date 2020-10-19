e80c3569aba03f7522c15603fef165f4702be321
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-10-19 13:17:11 -0500

    App Skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state ={
+      users: [],
+      user: null,
+      tasks: []
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <NewTask />
+          <List category={"toDo"} />
+          <List category={"inProgress"} />
+          <List category={"completed"} />
+          
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
