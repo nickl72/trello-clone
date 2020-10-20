fca144acfbee625b62e5be4418e916ae6afcae91
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-10-19 23:04:07 -0500

    added delete task and move task. Move task required changin task back to class

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -9,12 +9,12 @@ function List(props) {
 
     if(props.tasks) {
         props.tasks.map((task, id) => (
-            listItems.push(<Task task={task} key={id} />)
+            listItems.push(<Task task={task} key={id} handleDeleteTask={props.handleDeleteTask} handleMoveTask={props.handleMoveTask} handleSelectList={props.handleSelectList} catSelected={props.catSelected}/>)
         ))
     }
 
     return(
-        <div id={props.category} onMouseEnter={() => {props.onMouseEnter(props.category)}}>
+        <div id={props.category} /*onMouseEnter={() => {props.onMouseEnter(props.category)}}*/>
             <h2>{props.category}</h2>
             {listItems}
         </div>
