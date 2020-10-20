637e623b9ab519b7538b316f4dbb24a88c9ee2dd
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-10-20 14:39:54 -0400

    Converted Header to functional header, and added styling

--- src/App.js
+++ src/App.js
@@ -73,13 +73,13 @@ class App extends Component{
   
 
   render(){
-    console.log(this.state.tasks)
+    // console.log(this.state.tasks)
     const toDoList = [];
     const inProgressList = [];
     const completedList = [];
 
     this.state.tasks.map((task, id) => {
-      console.log(task)
+      // console.log(task)
       switch(task.category) {
         case "To-Do":
           toDoList.push(task);
@@ -91,14 +91,14 @@ class App extends Component{
           completedList.push(task);
           break;
         default:
-          console.error("Task category not recognized.")
-          console.error(task);
+          // console.error("Task category not recognized.")
+          // console.error(task);
       }
       return 0;
     })
 
-    console.log('todolist:')
-    console.log(toDoList)
+    // console.log('todolist:')
+    // console.log(toDoList)
     return (
       <div className="App">
         <Header users={this.state.users} login={this.login}/>
