495d2709f9cc1b37c9abf8a11575b65f348923e1
Chris Myers <myerschristopher@johndeere.com>
2020-10-20 13:08:16 -0500

    conditional list styling

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import Draggable from 'react-draggable';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 import Task from './Task'
 
-function List(props) {
 
+function List(props) {
+    const [listData, setlistData] = useState({
+        category: props.category,
+        tasks: props
+    })
+
+    let categoryStyles = '';
+    switch(listData.category) {
+        case "To-Do":
+            categoryStyles = 'background: coral;'
+            
+            break;
+        case "In Progress":
+            categoryStyles = 'background: lightgreen;'
+
+            break;
+        case "Completed":
+            categoryStyles = 'background: gold;'
+
+            break;
+        default:
+            console.error("Task category not recognized.")
+      }
+    
+    
+    
+    const StyledList = styled.div`
+        border: solid black 3px;
+        margin: 10px;
+        height: 70vh;
+        width: 100%;
+        overflow: scroll;
+        padding: 20px;
+        ${categoryStyles}
+    `
+    
     const listItems = [];
 
     if(props.tasks) {
@@ -14,10 +49,12 @@ function List(props) {
     }
 
     return(
-        <div id={props.category} /*onMouseEnter={() => {props.onMouseEnter(props.category)}}*/>
-            <h2>{props.category}</h2>
-            {listItems}
-        </div>
+        <StyledList>
+            <div id={props.category} /*onMouseEnter={() => {props.onMouseEnter(props.category)}}*/>
+                <h2>{props.category}</h2>
+                {listItems}
+            </div>
+        </StyledList>
     )
 }
 
