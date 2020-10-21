ae647d9cefc80e1b87ca72257f32a23d206d03c5
Chris Myers <myerschristopher@johndeere.com>
2020-10-21 09:57:18 -0500

    fix move task bug

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component{
   handleDragTask = (taskTitle,categoryName) => {
     console.log('Task: '+taskTitle);
     console.log('Category: '+categoryName);
-    let allTasks = this.state.tasks;
+    let allTasks = this.state.user.tasks;
     const foundTask = allTasks.filter(task => task.title === taskTitle)[0];
     const index = allTasks.indexOf(foundTask);
 
