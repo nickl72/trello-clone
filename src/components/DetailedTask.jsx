44a0a2304c7169d6833008a6f927de777d1b939b
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-10-21 14:45:52 -0500

    Styling Detailed Task modal

--- src/components/DetailedTask.jsx
+++ src/components/DetailedTask.jsx
@@ -17,26 +17,14 @@ const Div = styled.div`
     justify-content: center;
     align-items: center;
 `
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    background-color: #fff;
-    padding: 20px 30px;
-    border-radius: 10px;
-    box-shadow: 1px 1px 10px 1px black;
-    color: black;
-    text-align: center;
-    animation: ${slideAnimation} .5s;
-    width: 50vw;
-    height: 50vh;
-`
 
-const Card = styled.div`
+const DetailedCard = styled.div`
     display: flex;
     flex-direction: column;
     flex-wrap: nowrap;
     align-items: center;
-    width: 85%;
+    width: 400px;
+    height: 400px;
     border: solid black 1px;
     min-height: 50px;
     margin: 5px auto;
@@ -44,7 +32,7 @@ const Card = styled.div`
     border-radius: 5px;
     background: white;
     position: relative;
-    height: 85%;
+    animation: ${slideAnimation} .5s;
     
 
     .close{
@@ -74,14 +62,19 @@ const Card = styled.div`
     img{
         height: 15px;
     }
-    .description {
-        padding: 5px;
-        margin: 5px 5px 15px 5px;
+    .editTaskForm{
+        padding: 0;
+        margin: 0;
+        width: 100%;
+    }
+    .detailedDescription {
+        padding: 10px;
+        margin: 15px;
         border: solid gray 1px;
         border-radius: 2px;
         box-shadow: inset 0px 0px 2px gray;
     }
-    .dueDate {
+    .detailedDueDate {
         display: flex;
         align-items: center;
         justify-content: space-around;
@@ -172,62 +165,57 @@ function DetailedTask(props) {
                     private: detailedTask.private
                 })
             }
-            // console.log(detailedTask[targetName]);
-            // setDetailedTask({ 
-            //     [e.target.name] : e.target.value
-            // })
         }
     }
 
     return(
         <Div>
-            <Form>
-                <Card>
-                    <h3>{props.task.title}</h3>
-                    <button className="close" onClick={props.closeCard}>x</button>
-                    <p className="user">Created by: {props.task.user}</p>
-                    <form onSubmit={(e) => props.handleEditTask(e,props.task.taskId, detailedTask)}>
-                        <textarea className="description" rows= "5" cols="50" name="description" 
-                            value={detailedTask.description} onChange={onChange}></textarea>
-                        <div className="dueDate">
-                            <img src='https://www.flaticon.com/svg/static/icons/svg/37/37663.svg' alt="clock"/>
-                            <input type="date" name="dueDate" value={detailedTask.dueDate} onChange={onChange}></input>
-                            {props.whenDue(props.task.dueDate)}
-                        </div>
-                        <label for="private">Task Private:</label>
+            <DetailedCard>
+                <h3>{props.task.title}</h3>
+                <button className="close" onClick={props.closeCard}>x</button>
+                <p className="user">Created by: {props.task.user}</p>
+                <form className ="editTaskForm" onSubmit={(e) => props.handleEditTask(e,props.task.taskId, detailedTask)}>
+                    <textarea className="detailedDescription" rows= "5" cols="40" name="description" 
+                        value={detailedTask.description} onChange={onChange}></textarea>
+                    <div className="privateCheckbox" >
+                        <label for="private">Private Task:</label>
                         <input type='checkbox' name='private' onChange={() => {props.user && setDetailedTask({description: detailedTask.description, dueDate: detailedTask.dueDate, private: !detailedTask.private})}}
                             checked={detailedTask.private}
                         ></input>
-                        {props.user ?
-                            <EditButton><img src='https://www.flaticon.com/svg/static/icons/svg/860/860814.svg' alt="pencil/paper" />
-                            Edit Task</EditButton>
-                        : null }
-                    </form>
-                    {props.user ?
-                        <div className="editActions">
-                    <Button onClick={props.deleteTaskClick}>
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
-                        Delete Task
-                    </Button>
-                        <form onChange={(e)=> {props.setTaskData({newCategory:e.target.value})}} onSubmit={(e, task) => props.handleMoveTask(e, props.task, props.taskData.newCategory)} >
-                            <select name="category" defaultValue = {props.defaultCategory(props.taskData.category)}>
-                                <option value="To-Do">To-Do</option>
-                                <option value="In Progress">In Progress</option>
-                                <option value="Completed">Completed</option>
-                            </select> 
-                            <MoveButton type = "submit" className="moveButton">
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrows-move" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"/>
-                                </svg>
-                                Move Task
-                            </MoveButton>
-                        </form>
                     </div>
-                    :  null }
-                </Card>
-                    
-            </Form>
+                    <div className="detailedDueDate">
+                        <img src='https://www.flaticon.com/svg/static/icons/svg/37/37663.svg' alt="clock"/>
+                        <input type="date" name="dueDate" value={detailedTask.dueDate} onChange={onChange}></input>
+                        {props.whenDue(props.task.dueDate)}
+                    </div>
+                    {props.user ?
+                        <EditButton><img src='https://www.flaticon.com/svg/static/icons/svg/860/860814.svg' alt="pencil/paper" />
+                        Edit Task</EditButton>
+                    : null }
+                </form>
+                {props.user ?
+                    <div className="editActions">
+                <Button onClick={props.deleteTaskClick}>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg>
+                    Delete Task
+                </Button>
+                    <form onChange={(e)=> {props.setTaskData({newCategory:e.target.value})}} onSubmit={(e, task) => props.handleMoveTask(e, props.task, props.taskData.newCategory)} >
+                        <select name="category" defaultValue = {props.defaultCategory(props.taskData.category)}>
+                            <option value="To-Do">To-Do</option>
+                            <option value="In Progress">In Progress</option>
+                            <option value="Completed">Completed</option>
+                        </select> 
+                        <MoveButton type = "submit" className="moveButton">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrows-move" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"/>
+                            </svg>
+                            Move Task
+                        </MoveButton>
+                    </form>
+                </div>
+                :  null }
+            </DetailedCard>
         </Div>
     )
 
